1e859ecda90658a89a51380d2b5a08993ca20916
jumborin <jumborin@gmail.com>
2014-05-28 21:41:34 +0900

    ajax.coffeeのグローバル変数を全てローカル変数に変換。 表示形式フラグのGETの回数を3回から1回に修正

--- app/assets/javascripts/ajax.coffee
+++ app/assets/javascripts/ajax.coffee
@@ -1,34 +1,32 @@
+'use strict'
+
 angular.module('myapp', [
   'ui.bootstrap'
-])
-@dispFlg = -> [this.normal,this.number,this.color]
-@dispFlg.normal = -> [this.normalFlg,this.numberFlg,this.colorFlg]
-@dispFlg.number = -> [this.normalFlg,this.numberFlg,this.colorFlg]
-@dispFlg.color = -> [this.normalFlg,this.numberFlg,this.colorFlg]
+]).controller 'fibCtrl', ['$scope','$http', ($scope, $http) ->
+  dispFlg = -> [this.normal,this.number,this.color]
+  dispFlg.normal = -> [this.normalFlg,this.numberFlg,this.colorFlg]
+  dispFlg.number = -> [this.normalFlg,this.numberFlg,this.colorFlg]
+  dispFlg.color = -> [this.normalFlg,this.numberFlg,this.colorFlg]
+  $scope.fibSize=0
 
-@fibCtrl = ($scope, $http) ->
-　　$http.get("/dispFlg/normalFlg").success((data, status, headers, config) ->
-      dispFlg.normal=data
+  $http.get("/dispFlg/").success((data, status, headers, config) ->
+      dispFlg = data
       $scope.dispFlg = dispFlg.normal
     )
-  $http.get("/dispFlg/numberFlg").success((data, status, headers, config) ->
-      dispFlg.number=data
-    )
-  $http.get("/dispFlg/colorFlg").success((data, status, headers, config) ->
-      dispFlg.color=data
-    )
-  $scope.fibSize=0
-  
+
   $scope.size = ->
     url = "/fibSize/" + $scope.fibSize
     $http.get(url).success((data, status, headers, config) ->
       $scope.fibs = data
     ).error (data, status, headers, config) ->
       $scope.fibs = ""
+
   $scope.getNormalFlg = ->
     $scope.dispFlg = dispFlg.normal
+
   $scope.getNumberFlg = ->
     $scope.dispFlg = dispFlg.number
+
   $scope.getColorFlg = ->
     $scope.dispFlg = dispFlg.color
-
+]
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,7 +8,7 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index())
   }
-  
+
   def fibAction(fibSize:Int) = Action {
     if(0 <= fibSize && fibSize <= 100){
       Ok(Json.toJson(fib.take(fibSize)))
@@ -16,32 +16,28 @@ object Application extends Controller {
       BadRequest("Input value is at 0-100")
     }
   }
-  
+
   val fib: Stream[JsNumber] = JsNumber(0) #:: JsNumber(1) #:: fib.zip(fib.tail).map(p => JsNumber(p._1.value + p._2.value))
-  
-  val flgList:List[String] = List("normalFlg","numberFlg","colorFlg")
-  
-  def getDispFlgAction(dispFlg:String) = Action {
-    if(flgList.contains(dispFlg)){
-      Ok(Dao.selectDispFlg(dispFlg))
-    }else{
-      BadRequest("An incorrect value")
-    }
+
+  def getDispFlgAction = Action {
+      Ok(Dao.selectDispFlg)
   }
 }
-case class DispFlg(normalFlg:JsBoolean,numberFlg:JsBoolean,colorFlg:JsBoolean)
 
 import anorm._
 import play.api.db.DB
 import play.api.Play.current
 
 object Dao{
-  def selectDispFlg(dispFlg:String):JsValue = DB.withConnection{ implicit conn =>
-    return Json.toJson(
-        SQL("Select normalflg,numberflg,colorflg From disppatern Where name = '" + dispFlg + "' ")().map(
-            row => Map("normalFlg" ->JsBoolean(row[Boolean]("DISPPATERN.NORMALFLG")) ,
+  def selectDispFlg:JsValue = DB.withConnection{ implicit conn =>
+    val resultJsValueList:List[JsValue] =
+        (SQL("Select id,normalflg,numberflg,colorflg From disppatern ORDER BY id ASC")().map(
+            row => Json.toJson(Map("normalFlg" -> JsBoolean(row[Boolean]("DISPPATERN.NORMALFLG")) ,
                 "numberFlg" -> JsBoolean(row[Boolean]("DISPPATERN.NUMBERFLG")) ,
-                "colorFlg" ->JsBoolean(row[Boolean]("DISPPATERN.COLORFLG")))).toList.apply(0))
+                "colorFlg" -> JsBoolean(row[Boolean]("DISPPATERN.COLORFLG"))))).toList
+        )
+    return Json.toJson(Map("normal" -> resultJsValueList(0),
+         "number" -> resultJsValueList(1),
+         "color" -> resultJsValueList(2)))
   }
 }
-
